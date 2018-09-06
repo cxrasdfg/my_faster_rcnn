36de8c96b53a9a1587f0834463eb33e4bcc327de
theppsh <734331254@qq.com>
2018-09-06 16:07:48 +0800

    modify config

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 # coding=UTF-8
 
 class CFG():
-    voc_dir='/root/workspace/data/VOC2007_2012/VOCdevkit/VOC2007'
+    voc_dir='/root/workspace/D/VOC2007_2012/VOCdevkit/VOC2007'
 
     train_feat_dir='./features/vgg16/train/'
     train_use_offline_feat=False
@@ -25,9 +25,12 @@ class CFG():
     img_shorter_len=600
     img_longer_len=1000
 
+    batch_size=1
+    num_workers=0
+
     rand_seed=0
     epochs=35
-    lr=1e-6
+    lr=1e-3
     weight_decay=0.0005
     use_adam=False
     rpn_sigma=1.
